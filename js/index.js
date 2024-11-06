22dc7f29270d6f23ca88f80e1a04ac6565652e8b
phewson16 <peterhewson16@gmail.com>
2024-11-06 12:45:26 -0500

    Delete button works, display button not

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-console.log('js');
 
-function addIngredient() {
+function addIngredient(event) {
+    event.preventDefault(); // Had to Google how to stop page from refreshing on each submission and use of event, wouldn't work otherwise
 
+    // User input of Ingredients
     const ingredientInput = document.getElementById('ingredientInput');
     const quantityInput = document.getElementById('quantityInput');
     const unitInput = document.getElementById('unitMeasurement');
@@ -9,12 +10,11 @@ function addIngredient() {
     const ingredientText = ingredientInput.value;
     const quantityText = quantityInput.value;
     const unitText = unitInput.value;
-
     const list = document.getElementById('ingredientList');
 
     // Create the list item
     const listItem = document.createElement('p');
-    
+
     // Create the checkbox
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -23,16 +23,69 @@ function addIngredient() {
     const textSpan = document.createElement('span');
     textSpan.textContent = `${ingredientText} - ${quantityText} ${unitText}`;
         
-    // Append the checkbox and text to the list item
+    // Append checkbox and text to listItem
     listItem.appendChild(checkbox);
     listItem.appendChild(textSpan);
         
-    // Append the list item to the list
+    // Append listItem to list
     list.appendChild(listItem);
 
     // Clear the input fields after adding the ingredient
     ingredientInput.value = "";
     quantityInput.value = "";
 }
+
+// Delete items selected in checklist
+function deleteIngredient(event) {
+    console.log('delete');
+    event.preventDefault(); // Had to Google how to stop page from refreshing on each submission and use of event, wouldn't work otherwise
+
+    const list = document.getElementById('ingredientList');
+    const items = list.getElementsByTagName('p');
+
+    // Collect items to be removed
+    const itemsToRemove = [];
+    for (let item of items) {
+        const checkbox = item.getElementsByTagName('input')[0];
+        if (checkbox.checked) {
+            itemsToRemove.push(item);
+        }
+    }
+
+    // Remove collected items from the list
+    for (let item of itemsToRemove) {
+        list.removeChild(item);
+    }
+
+}
+
+
+// Using the checklist, display all SELECTED ingredients, recipe name
+function displayIngredient(event) {
+    console.log('display');
+    event.preventDefault(); // Had to Google how to stop page from refreshing on each submission and use of event, wouldn't work otherwise
+
+    const recipeName = document.getElementById('recipeName');
+    const recipeNameText = recipeName.value;
+
+    const list = document.getElementById('ingredientList');
+    const items = list.getElementsByTagName('ul');
+
+    const ingredientText = ingredientInput.value;
+
+    const selectedIngredients = [];
+    for (let item of items) {
+        const checkbox = item.getElementsByTagName('input')[0];
+        if (checkbox.checked) {
+            selectedIngredients.push(item.getElementsByTagName('span')[0].textContent);
+        }
+    }
+
+    const displayArea = document.getElementById('selectedIngredients');
+    displayArea.textContent = `${recipeNameText}\n${selectedIngredients.join('\n')}`;
+}
+
+
+
 
 
